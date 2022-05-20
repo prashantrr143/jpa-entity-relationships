516562fbfe67d52775e0dd2c43778ac0494a419b
Prashant Singh <prashant.siet153@gmail.com>
2022-05-20 13:53:44 +0530

    First Commit

---
+++ src/main/java/com/prashant/learning/jpa/entity/relations/JpaEntityRelationshipsApplication.java
@@ -0,0 +1,13 @@
+package com.prashant.learning.jpa.entity.relations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaEntityRelationshipsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaEntityRelationshipsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prashant/learning/jpa/entity/relations/JpaEntityRelationshipsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prashant.learning.jpa.entity.relations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaEntityRelationshipsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
